25a3ceea7818ac20878967d2c439eee6b675f634
Vinish Bhaskar <vinish.bhaskar@outlook.com>
2023-05-19 11:31:27 +0530

    Error Page Auto Redirect

--- quiz-app/src/pages/ErrorPage.js
+++ quiz-app/src/pages/ErrorPage.js
@@ -1,22 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {FaExclamationTriangle, FaArrowRight} from "react-icons/fa"
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaExclamationTriangle, FaArrowRight } from "react-icons/fa";
 
+const Error = () => {
+  const navigate = useNavigate();
 
-function Error() {
+  useEffect(() => {
+    const redirectTimeout = setTimeout(() => {
+      navigate("/");
+    }, 5000);
+
+    return () => clearTimeout(redirectTimeout);
+  }, [navigate]);
+
   return (
     <div className="error-page">
       <div className="message">
         <div className="icon">
-          <FaExclamationTriangle/>
+          <FaExclamationTriangle />
         </div>
         <span>Page not found!</span>
       </div>
       <Link className="error-link" to="/">
-        <span>Go Home Page</span> <FaArrowRight/>
+        <span>Go to Home Page</span> <FaArrowRight />
       </Link>
     </div>
   );
-}
+};
 
 export default Error;
